36634a0881313df787df0d0c319d286eb528b716
leandro-hbs <leandro.henrique.batista.da.silva@gmail.com>
2024-08-29 18:34:15 -0300

    ci(workflow): add validate terraform for all directories

--- resources.tf
+++ resources.tf
@@ -2,8 +2,3 @@ resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
   tags   = var.tags
 }
-
-resource "aws_s3_bucket" "example2" {
-  bucket = "${var.bucket_name}-2"
-  tags   = var.tags
-}
